ba0b490ebf92a644e17de294209a7c359e4b1ce1
mdrcastaneda <mrosario.castaneda@gmail.com>
2026-03-24 11:13:39 -0500

    Actualización script de R

--- scripts/mineria_datos_gbif.R
+++ scripts/mineria_datos_gbif.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
+
+# Vamos a hacer cambios en el archivo
+#
 data <- read.csv("~/Desktop/CLASES/Icesi/*Analisis de Datos II (Bioinformática)/Talleres/mineria_datos_gbif/data/gbifdata_tfuscater.csv")
 
 #### Explore su archivo resultante y responda:
-#- En qué departamentos se ha registrado la especie, y cuántos registros 
+#- En qué departamentos se ha registrado la especie, y cuántos registros
 # hay para cada departamento?
 table(data$stateProvince)
 
@@ -12,7 +15,7 @@ table(data$stateProvince)
 data <- data %>%
   mutate(stateProvince = str_replace(stateProvince, "Distrito Capital de Bogotá", "Bogotá, D.C.")) %>%
   mutate(stateProvince = str_replace(stateProvince, "Bogota Capital District", "Bogotá, D.C."))
-  
+
 table(data$stateProvince)
 
 # En qué países se encuentran los especímenes?
@@ -20,7 +23,7 @@ table(data$stateProvince)
 table(data$publishingCountry)
 
 # de qué años
- 
+
 table(data$year)
 
 
@@ -28,13 +31,13 @@ ti <- name_backbone(name = "Turdus ignobilis", rank = "species")
 ti$usageKey
 
 tignobilis <- occ_search(taxonKey =  ti$usageKey,
-                        country = "CO", limit = 1000, 
-                        hasCoordinate = TRUE, 
-                        hasGeospatialIssue = FALSE, 
+                        country = "CO", limit = 1000,
+                        hasCoordinate = TRUE,
+                        hasGeospatialIssue = FALSE,
                         fields = c("key", "taxonKey", "scientificName",
                                    "decimalLongitude", "decimalLatitude",
                                    "eventDate", "basisOfRecord",
-                                   "countryCode", "stateProvince", 
+                                   "countryCode", "stateProvince",
                                    "locality", "datasetKey",
                                    "publishingCountry", "year",
                                    "occurrenceStatus"))
